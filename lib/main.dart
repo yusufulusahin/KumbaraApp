9492a31a134be155d97a079e70b34c8cd3609bbd
Yusuf <yusufulusahinn@gmail.com>
2024-07-06 14:42:02 +0300

    Login ve Sign in Screen'in Uİ tasarımı bitti

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter KumbaraApp',
       home: LoginScreen(),
     );
   }
