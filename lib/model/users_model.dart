788511a85953e0127453139d31639e2271fee4e6
Yusuf <yusufulusahinn@gmail.com>
2024-07-08 10:24:31 +0300

    User Model

---
+++ lib/model/users_model.dart
@@ -0,0 +1,16 @@
+class Users {
+  final String adSoyad;
+  final String telNo;
+  final String il;
+  final String ilce;
+  final String sifre;
+  final String cihazid;
+
+  Users(
+      {required this.adSoyad,
+      required this.telNo,
+      required this.il,
+      required this.ilce,
+      required this.sifre,
+      required this.cihazid});
+}
