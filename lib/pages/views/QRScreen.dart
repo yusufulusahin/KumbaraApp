f63010d1094c12d5530fb34b2969c7958e0c7f87
Yusuf <yusufulusahinn@gmail.com>
2024-07-13 09:25:37 +0300

    Barkod Okuma Fonksiyonu Tamamlandı.

--- lib/pages/views/QRScreen.dart
+++ lib/pages/views/QRScreen.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:testt/component/CustomSnackBar.dart';
+
+import 'QRResultScreen.dart';
 
 class Qrscreen extends StatefulWidget {
   const Qrscreen({super.key});
@@ -8,10 +15,43 @@ class Qrscreen extends StatefulWidget {
 }
 
 class _QrscreenState extends State<Qrscreen> {
+  MobileScannerController scannerController = MobileScannerController();
+
+  @override
+  void initState() {
+    //this._screenWasOpened();
+    super.initState();
+  }
+
+  void _onDetect(BarcodeCapture barcodeCapture) {
+    final List<Barcode> barcodes = barcodeCapture.barcodes;
+    if (barcodes.isNotEmpty) {
+      final String code = barcodes.first.rawValue ?? "Kod Bulunamadı";
+      showCustomSnackBar(context, 'Tarama Sonucu $code');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Placeholder(),
+      backgroundColor: Colors.black.withOpacity(0.5),
+      appBar: AppBar(
+        title: const Padding(
+          padding: EdgeInsets.only(bottom: 20),
+          child: Text(
+            'QR Tarayıcı',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+          ),
+        ),
+        elevation: 0.0,
+      ),
+      body: MobileScanner(controller: scannerController, onDetect: _onDetect),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          scannerController.toggleTorch();
+        },
+        child: Icon(Icons.flash_on),
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import app_set_id
 import cloud_firestore
 import firebase_core
+import mobile_scanner
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppSetIdPlugin.register(with: registry.registrar(forPlugin: "AppSetIdPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
 }
